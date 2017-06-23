af668ebac533f0684b77158c85dcd6c142a64994
Leonardo Ribeiro <lviniciusribeiro@yahoo.com.br>
2017-06-23 13:40:52 -0300

    Iniciado Projeto - Gerenciado de Projetos

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<title>GR Projetos</title>
+		<meta charset="UTF-8">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css">
+		<link rel="stylesheet" type="text/css" href="materialize/css/style.css">
+	</head>
+		<body>
+			<div class="container" style="border-style: black;">
+			<div class="row">
+				<div class="col m12 s12">
+					<center>
+						<h1>Gerenciamento de Projetos</h1>					
+					</center>		
+				</div>		
+			</div>
+			<div class="row">
+				<div class="col m5 s12">
+					<h5>Projeto designado - <b>GR Projetos</b></h5>
+				</div>
+				<div class="col m2">
+				<center>
+					<i class="material-icons" style='font-size: 60px; color: #0d47a1;'>assessment</i>
+				</center>
+				</div>		
+				<div class="col m5 s12">
+					<h5>Responsável - <b>Leonardo Ribeiro</b></h5>
+				</div>		
+			</div>
+					<div style="border-radius: 20px; border-color: black; border-style: solid; height: 10cm;">
+						<div class="row">
+							<div class="col m12">
+								<div class="center">	
+									<h3>Andamento do Projeto</h3>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div style="margin-left: 1cm;">
+								<p>Inicio do Projeto</p>	
+							</div>
+						</div>
+					</div>
+			</div>
+
+
+		</body>
+</html>
