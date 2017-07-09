3555ec12419c5b6ca60aec5f7c70b5a157a3396e
Leonardo Ribeiro <lviniciusribeiro@yahoo.com.br>
2017-07-09 00:36:02 -0300

    add autoload.php

---
+++ controller/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+	require_once __DIR__ . ('/requisicoes.php');
+	require_once __DIR__ .('/AtualizaProjeto.php');
+	require_once __DIR__ .('/conexao.php');
+
+/*
+	$arquivos = glob("{*.php}", GLOB_BRACE);
+	foreach($arquivos as $arqs){
+	require_once __DIR__. "/". ($arqs);
+	}
+*/
+?>
