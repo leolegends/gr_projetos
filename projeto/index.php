0d79e6b01170349ee27ee2da5ae6c791721d0359
Leonardo Ribeiro <lviniciusribeiro@yahoo.com.br>
2017-07-13 12:33:34 -0300

    Create screen for token

---
+++ projeto/index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>GR Projetos</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=600, initial-scale=1">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="../materialize/css/materialize.min.css">
+		<link rel="stylesheet" type="text/css" href="../materialize/css/style.css">
+		<script type="text/javascript" src="../materialize/js/materialize.min.js"></script>
+		<script type="text/javascript" src="../materialize/js/jquery.min.js"></script>
+		<script type="text/javascript" src="../materialize/js/config.js"></script>
+	</head>
+		<body>
+			<div class='center'>
+
+				<div class="row container">
+					<form method="post" action="index.php">
+					<div class="col m12">
+					<label style='font-size: 50px;'>TOKEN <i class="material-icons" style='font-size: 50px;'>fingerprint</i></label>
+					<input type='password' name='token' style='height: 4cm; font-size: 50px; text-align: center;' maxlength="5">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col m12 center">
+						<button class='btn green darken-4' style='color: white'>Verificar Token</button>
+					</div>
+				</div>
+				</form>
+			</div>
+		</body>
+</html>
